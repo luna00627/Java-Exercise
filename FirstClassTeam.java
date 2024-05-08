6dd9d3a8849a090d2077aaa609ff82ebabb0a9ea
ct01157041 <01157041@email.ntou.edu.tw>
2024-05-08 16:02:53 +0800

    01157041
    01157041

--- FirstClassTeam.java
+++ FirstClassTeam.java
@@ -3,5 +3,6 @@ public class FirstClassTeam{
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157021\"\n姓名：\"邱子玲\""); 
         System.out.println("\"學號：\"01157038\"\n姓名：\"郭泳函\""); 
+        System.out.println("\"學號：\"01157041\"\n姓名：\"蔡沂呈\"");
     }
 }
