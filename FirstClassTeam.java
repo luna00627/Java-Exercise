f1aa1ed3cc9d7cf678c561163ca861f15693d0aa
gelato0423 <152263085+gelato0423@users.noreply.github.com>
2024-05-08 16:00:03 +0800

    han

--- FirstClassTeam.java
+++ FirstClassTeam.java
@@ -2,5 +2,6 @@ public class FirstClassTeam{
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157021\"\n姓名：\"邱子玲\""); 
+        System.out.println("\"學號：\"01157038\"\n姓名：\"郭泳函\""); 
     }
 }
