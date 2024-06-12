bd6e8fddb5f1a69ae426cad821882570c492708d
luna00627 <az991130627@gmail.com>
2024-06-13 04:31:43 +0800

    oganizeOK
    完全整理好，但還沒整合

---
+++ Java-Exercise/src/Gif.java
@@ -0,0 +1,31 @@
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+
+public class Gif {
+    public static void main(String[] args) throws Exception {
+        BufferedImage image1 = ImageIO.read(new File("C:/java-project/pig1.png"));
+        BufferedImage image2 = ImageIO.read(new File("C:/java-project/pig2.png"));
+        BufferedImage image3 = ImageIO.read(new File("C:/java-project/pig3.png"));
+        BufferedImage image4 = ImageIO.read(new File("C:/java-project/pig4.png"));
+        AnimatedGifEncoder e = new AnimatedGifEncoder();
+        //生成的图片路径
+        e.start(new FileOutputStream("C:/java-project-test/gif.gif"));
+        //图片宽高
+        e.setSize(300, 190);
+        //图片之间间隔时间
+        e.setDelay(400);
+        //重复次数 0表示无限重复 默认不重复
+        e.setRepeat(0);
+        //添加图片
+        e.addFrame(image1);
+        e.addFrame(image2);
+        e.addFrame(image3);
+        e.addFrame(image4);
+        e.finish();
+    }
+}
---
+++ Java-Exercise/src/HomePage.java
@@ -0,0 +1,88 @@
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+import javax.swing.SwingUtilities;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class HomePage extends JFrame {
+    JMenuBar menuBar;
+    JMenu menu, subMenu;
+    JMenuItem item1, item2, stickerWork, gifWork;
+
+    public HomePage() {
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("首頁");
+        setLocation(20, 30);
+        setSize(1000, 700);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        menuBar = new JMenuBar();
+        menu = new JMenu("菜單");
+        subMenu = new JMenu("我的作品集");
+
+        item1 = new JMenuItem("貼圖生成");
+        item2 = new JMenuItem("GIF生成");
+        stickerWork = new JMenuItem("貼圖作品");
+        gifWork = new JMenuItem("GIF作品");
+
+        item1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 在此處理貼圖生成的功能
+                JOptionPane.showMessageDialog(null, "執行貼圖生成功能");
+            }
+        });
+
+        item2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 在此處理GIF生成的功能
+                JOptionPane.showMessageDialog(null, "執行GIF生成功能");
+            }
+        });
+
+        stickerWork.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 在此處理我的作品集中的貼圖作品功能
+                JOptionPane.showMessageDialog(null, "打開我的貼圖作品集");
+            }
+        });
+
+        gifWork.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 在此處理我的作品集中的GIF作品功能
+                JOptionPane.showMessageDialog(null, "打開我的GIF作品集");
+            }
+        });
+
+        /*menu.setPreferredSize(new Dimension(200, 100)); //設定選項大小
+        subMenu.setPreferredSize(new Dimension(200, 100));
+        menu.setFont(new Font("", Font.BOLD, 16));//設定字體大小
+        subMenu.setFont(new Font("", Font.BOLD, 16));*/
+
+        menu.add(item1);
+        menu.add(item2);
+        menu.add(subMenu);
+        subMenu.add(stickerWork);
+        subMenu.add(gifWork);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                HomePage homePage = new HomePage();
+                homePage.setVisible(true);
+            }
+        });
+    }
+}
+
