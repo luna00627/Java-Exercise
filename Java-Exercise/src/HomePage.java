70a6d449e3cc2e064040ed0dd9d0ec336efa7b39
gelato0423 <152263085+gelato0423@users.noreply.github.com>
2024-06-14 23:18:23 +0800

    Update HomePage.java

--- Java-Exercise/src/HomePage.java
+++ Java-Exercise/src/HomePage.java
@@ -1,19 +1,35 @@
+package project;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.WindowConstants;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.ImageIcon;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
+
 
 public class HomePage extends JFrame {
-    JMenuBar menuBar;
-    JMenu menu, subMenu;
-    JMenuItem item1, item2, stickerWork, gifWork;
+    private JMenuBar menuBar;
+    private JMenu menu, subMenu;
+    private JMenuItem item1, item2, stickerWork, gifWork;
+    private JPanel mainPanel;
+    private ImageIcon gifIcon;
+    private JLabel gifLabel;
 
     public HomePage() {
         initUI();
@@ -23,7 +39,7 @@ public class HomePage extends JFrame {
         setTitle("首頁");
         setLocation(20, 30);
         setSize(1000, 700);
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         menuBar = new JMenuBar();
         menu = new JMenu("菜單");
@@ -34,17 +50,48 @@ public class HomePage extends JFrame {
         stickerWork = new JMenuItem("貼圖作品");
         gifWork = new JMenuItem("GIF作品");
 
-        item1.addActionListener(new ActionListener() {
+        Font menuFont = new Font("Microsoft JhengHei", Font.PLAIN, 14);
+        menu.setFont(menuFont);
+        subMenu.setFont(menuFont);
+        item1.setFont(menuFont);
+        item2.setFont(menuFont);
+        stickerWork.setFont(menuFont);
+        gifWork.setFont(menuFont);
+
+        /*item1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 啟動照片編輯器，在此處理貼圖生成的功能
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        JFrame photoEditorFrame = new JFrame("照片編輯器");
+                        photoEditorFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        photoEditorFrame.setSize(800, 600);
+
+                        // 創建 PhotoEditor 的實例
+                        PhotoEditor photoEditor = new PhotoEditor();
+                        photoEditorFrame.setContentPane(photoEditor.createContentPane());
+                        
+                        photoEditorFrame.setVisible(true);
+                    }
+                });
+            }
+        });*/
+
+         item1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // 在此處理貼圖生成的功能
                 JOptionPane.showMessageDialog(null, "執行貼圖生成功能");
             }
         });
 
+
         item2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // 在此處理GIF生成的功能
-                JOptionPane.showMessageDialog(null, "執行GIF生成功能");
+                try {
+                    generateGIF();
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null, "無法生成 GIF：" + ex.getMessage());
+                }
             }
         });
 
@@ -58,14 +105,9 @@ public class HomePage extends JFrame {
         gifWork.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // 在此處理我的作品集中的GIF作品功能
-                JOptionPane.showMessageDialog(null, "打開我的GIF作品集");
+                showGIF();
             }
         });
-
-        /*menu.setPreferredSize(new Dimension(200, 100)); //設定選項大小
-        subMenu.setPreferredSize(new Dimension(200, 100));
-        menu.setFont(new Font("", Font.BOLD, 16));//設定字體大小
-        subMenu.setFont(new Font("", Font.BOLD, 16));*/
 
         menu.add(item1);
         menu.add(item2);
@@ -74,6 +116,43 @@ public class HomePage extends JFrame {
         subMenu.add(gifWork);
         menuBar.add(menu);
         setJMenuBar(menuBar);
+
+        mainPanel = new JPanel(new BorderLayout());
+        gifLabel = new JLabel();
+        mainPanel.add(gifLabel, BorderLayout.CENTER);
+        setContentPane(mainPanel);
+    }
+
+    private void generateGIF() throws IOException {
+        BufferedImage image1 = ImageIO.read(new File("C:/java-project/pig1.png"));
+        BufferedImage image2 = ImageIO.read(new File("C:/java-project/pig2.png"));
+        BufferedImage image3 = ImageIO.read(new File("C:/java-project/pig3.png"));
+        BufferedImage image4 = ImageIO.read(new File("C:/java-project/pig4.png"));
+        
+        AnimatedGifEncoder encoder = new AnimatedGifEncoder();
+        encoder.start(new FileOutputStream("C:/java-project-test/gif.gif"));
+        encoder.setSize(300, 190);
+        encoder.setDelay(400);
+        encoder.setRepeat(0);
+        encoder.addFrame(image1);
+        encoder.addFrame(image2);
+        encoder.addFrame(image3);
+        encoder.addFrame(image4);
+        encoder.finish();
+        
+        JOptionPane.showMessageDialog(null, "GIF生成成功！");
+    }
+
+    private void showGIF() {
+        String gifFilePath = "C:/java-project-test/gif.gif"; // GIF 檔案的路徑
+        File gifFile = new File(gifFilePath);
+        
+        if (gifFile.exists() && gifFile.isFile()) {
+            gifIcon = new ImageIcon(gifFilePath);
+            gifLabel.setIcon(gifIcon);
+        } else {
+            JOptionPane.showMessageDialog(null, "找不到 GIF 檔案：" + gifFilePath);
+        }
     }
 
     public static void main(String[] args) {
@@ -85,4 +164,3 @@ public class HomePage extends JFrame {
         });
     }
 }
-
